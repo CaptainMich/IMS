4ee9bcc0a8fe41fbe6462ea737ed89c0defd60ee
grilluz <andrea.grillo96@gmail.com>
2016-09-24 23:15:56 +0200

    check if grid element exist

--- site/homepage/static/js/content.js
+++ site/homepage/static/js/content.js
@@ -1,7 +1,14 @@
 col_resize = function(){
-    var h= $('#rg_grid').outerHeight();
-	console.log(h);
-	$('#sk_content').height(h+100);
+	var window_h = $(window).height();
+	//console.log(window_h);
+	
+    if($('#rg_grid').length) { //if it exists
+		var h= $('#rg_grid').outerHeight();
+		if(window_h < h) {
+			$('#sk_content').height(h+100);
+			console.log(h);
+		}
+	}
 };
 
 $(window).load(col_resize);
