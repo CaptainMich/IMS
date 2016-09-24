fd10e4254df5282e813f23f47e25924db81b3d89
grilluz <andrea.grillo96@gmail.com>
2016-09-24 11:24:55 +0200

    post list js responding to window resize events

--- site/homepage/static/js/post_list.js
+++ site/homepage/static/js/post_list.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
+img_resize = function(){
     var w = $('.wrap-image').width();
 	$('.wrap-image').height(w);
 	$('.preview-image').height(w);
-});
+};
+
+$(window).resize(img_resize);
+$(document).ready(img_resize);
